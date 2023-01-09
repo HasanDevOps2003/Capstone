5d02847e0c9e85ee4c3331d6493a85f563ac3c7f
HasanDevOps2003 <muhammedhasan483@gmail.com>
2023-01-09 22:55:23 +0000

    upload docker added

---
+++ upload_docker.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# This file tags and uploads an image to Docker Hub
+
+# Assumes that an image is built via `run_docker.sh`
+
+# Step 1:
+# Create dockerpath
+# dockerpath=<your docker ID/path>
+dockerpath="muhmmadhasan2003/capstoneproject"
+
+# Step 2:  
+# Authenticate & tag
+docker login -u $DOCKER_LOGIN -p $DOCKER_PASSWORD
+docker tag capstoneproject $dockerpath
+echo "Docker ID and Image: $dockerpath"
+
+# Step 3:
+# Push image to a docker repository
+docker push $dockerpath
