a29fde4c6868fe8d2853e4c2c31c3ff6ea07a158
HasanDevOps2003 <muhammedhasan483@gmail.com>
2023-01-10 14:40:35 +0000

    eks script update

--- install_eksctl.sh
+++ install_eksctl.sh
@@ -1,11 +1,4 @@
 #!/usr/bin/env bash
-
-COMMAND="eksctl"
-
-test -e ./bin/${COMMAND} ||
-    {
-        curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C ./bin
-        chmod +x ./bin/${COMMAND}
-    }
-
-echo "./bin/${COMMAND}: $(./bin/${COMMAND} version)"
+curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version
